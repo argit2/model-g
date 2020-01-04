d34473043d91de98d3b8fb595c7c40b0e4e900b4
argit2 <38925828+argit2@users.noreply.github.com>
2020-01-04 18:09:35 -0300

    Updates

---
+++ precache-manifest.178518d6bc0954faca7bb8f9ef30702b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9650002dee01a8db6dd03c45e939e88",
+    "url": "/model-g/index.html"
+  },
+  {
+    "revision": "efb6badc30816070bf6b",
+    "url": "/model-g/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "03bb98c402e023452dbc",
+    "url": "/model-g/static/css/main.04f1b79c.chunk.css"
+  },
+  {
+    "revision": "efb6badc30816070bf6b",
+    "url": "/model-g/static/js/2.723de48c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/model-g/static/js/2.723de48c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03bb98c402e023452dbc",
+    "url": "/model-g/static/js/main.6ad65875.chunk.js"
+  },
+  {
+    "revision": "e3190b25d777f0f1f4fb",
+    "url": "/model-g/static/js/runtime-main.bc4063f2.js"
+  }
+]);
